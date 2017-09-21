92c090d89835d6d4e2943e579aef42a17b65e7cc
MakeleAndzelo <97.piotrowski.michal@gmail.com>
2017-09-21 14:40:38 +0200

    Added custom columns to messages post type

--- includes/custom_post_types.php
+++ includes/custom_post_types.php
@@ -4,6 +4,31 @@ $contactForm = get_option('active_form');
 
 if($contactForm) {
     add_action('init', 'messagesCustomPostType');
+
+    add_action('manage_sunset_messages_posts_custom_column', 'manageMessagesColumnsContent', 10, 2);
+    add_filter('manage_sunset_messages_posts_columns', 'manageMessagesColumns');
+}
+
+function manageMessagesColumnsContent($column, $postId)
+{
+    switch($column) {
+        case 'excerpt':
+            echo get_the_excerpt();
+            break;
+        case 'email':
+            echo 'email';
+            break;
+    }
+}
+
+function manageMessagesColumns()
+{
+    return [
+        'title' => 'Title',
+        'excerpt' => 'Excerpt',
+        'email' => 'Email',
+        'date' => 'Date',
+    ];
 }
 
 function messagesCustomPostType()
@@ -26,5 +51,5 @@ function messagesCustomPostType()
         'supports' => ['title', 'editor', 'author'],
     ];
 
-    register_post_type('sunset-messages', $args);
+    register_post_type('sunset_messages', $args);
 }
